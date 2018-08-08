d01624f2e98a7e4f0ac7c6ec8b999357d71372c4
Raul <raulch1998@gmail.com>
2018-08-08 11:53:26 -0400

    43. Logging Out User

---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+// Create a cookie/token for the user signed in
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+// Pull back the token from the user
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback"
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleID: profile.id }).then(existingUser => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({ googleID: profile.id })
+                        .save()
+                        .then(user => done(null, existingUser));
+                }
+            });
+        }
+    )
+);
